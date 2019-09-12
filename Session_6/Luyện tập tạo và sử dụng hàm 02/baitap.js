f9b0c5f0e2174530e31cbb9f91507b554ca8364d
BuiHoang <buihoang@Hoangs-MacBook-Pro.local>
2019-09-12 13:49:20 +0700

    add.

--- Session_6/Luyện tập tạo và sử dụng hàm 02/baitap.js
+++ Session_6/Luyện tập tạo và sử dụng hàm 02/baitap.js
@@ -10,7 +10,7 @@ function circle() {
     let radius = parseInt(inputTradius);
     let area = radius * radius * 3.14;
     let perimeter = radius * 3.14 * 2;
-    document.write(" Area: " + area + "<br>" + " Perimeter: " + perimeter);
+    alert(" Area: " + area  + "   Perimeter: " + perimeter);
 }
 
 function factorialize() {
@@ -23,3 +23,57 @@ function factorialize() {
     alert(" factorialize is: " + num);
 }
 factorialize(num);
+
+function isNumbers() {
+    let inputNumbers = Number(prompt("Nhập vào kí tự: "));
+        if (inputNumbers >= 0 && inputNumbers <= 9) {
+            alert("true")
+        }else {
+            alert("false");
+        }
+}
+
+
+function minNumbers() {
+    let Number1 = Number(prompt("Nhập số thứ nhất: "))
+    let Nubmer2 = Number(prompt("Nhập số thứ hai: "));
+    let Number3 = Number(prompt("Nhập số thứ ba: "));
+    let arr = [];
+    arr.push(Number1, Nubmer2, Number3);
+    let Min = arr[0];
+    for (i = 0; i < arr.length; i++) {
+        if (Min >= arr[i]) {
+            Min = arr[i];
+        }
+    }
+    alert("Số nhỏ nhất là: " + Min);
+}
+
+function integer() {
+    let inputNumber = Number(prompt("Nhập số bạn cần kiểm tra: "));
+    if (inputNumber > 0) {
+        alert("Đây là số nguyên dương: True ");
+    }else {
+            if (inputNumber <= 0){
+                alert("Đây không là số nguyên dương: Fasle ");
+            }
+        }
+    }
+
+    function swap(Number1,Number2) {
+    let num;
+    num = Number1;
+    Number1=Number2;
+    Number2=num;
+
+    alert(Number1);
+    alert(Number2);
+}
+function clickNum() {
+    let num1 = Number(prompt("Nhập vào số thứ nhất: "));
+    let num2 = Number(prompt("Nhập vào số thứ hai: "));
+    swap(num1,num2);
+}
+
+
+
