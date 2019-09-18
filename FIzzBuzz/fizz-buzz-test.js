4a4a5181314a886e4a9020319b4de36226ddf5dd
buihoang <hoangbuivan0962654936@gmail.com>
2019-09-18 20:06:02 +0700

    first test key

--- FIzzBuzz/fizz-buzz-test.js
+++ FIzzBuzz/fizz-buzz-test.js
@@ -1,3 +1,4 @@
-QUnit.test( "hello test", function( assert ) {
-    assert.ok( 1 == "1", "Passed!" );
+QUnit.test( "class FizzBuzzNumber có tồn tại", function( assert ) {
+    // assert.ok( 1 == "1", "Passed!" );
+    let fizzBuzzNumber = new FizzBuzzNumber(1);
 });
