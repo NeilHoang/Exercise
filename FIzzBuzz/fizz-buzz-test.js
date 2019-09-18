e6897012390dbaaeb4f8581eda7b6f1c7d0b9f07
buihoang <hoangbuivan0962654936@gmail.com>
2019-09-18 20:16:20 +0700

    test key fizzbuzznumber

--- FIzzBuzz/fizz-buzz-test.js
+++ FIzzBuzz/fizz-buzz-test.js
@@ -1,4 +1,4 @@
 QUnit.test( "class FizzBuzzNumber có tồn tại", function( assert ) {
-    // assert.ok( 1 == "1", "Passed!" );
     let fizzBuzzNumber = new FizzBuzzNumber(1);
+    assert.ok(fizzBuzzNumber != null);
 });
